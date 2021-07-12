1c000f82e34029dc297298f34f62c85b117ca39a
Raghu <74982159+Raghunath-Tiwari@users.noreply.github.com>
2021-07-12 23:30:39 +0530

    Add files via upload

---
+++ product2no.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"product of these two numbers is: "<<a*b;
+return 0;
+}
